4a629a9ee50a3f3c350804011f8f52ee3fbd9302
lalauya1122 <lalauya1122@gmail.com>
2017-12-26 01:45:05 +0800

    git clone https://github.com/lalauya1122/LineAlphatJS

--- src/bot.js
+++ src/bot.js
@@ -2,11 +2,11 @@ const LineConnect = require('./connect');
 let LINE = require('./main.js');
 
 const auth = {
-	authToken: ' token disini',
-	certificate: 'cert here',
+	authToken:'EooTJvrRSwVvtHHNOF1c.BEl9dt8wGdIbrthb/4/33a.ywuRZ5O2ag5r3pizKF7MiGWnwVqXumNdODslTlROZ+o=',
+	certificate:'fe0261a0b2d26a396495805608b6b01e38ab0f75b7e3b1c660e0b8d0416b1ebd',
 }
-// let client =  new LineConnect(auth);
-let client =  new LineConnect();
+ let client =  new LineConnect(auth);
+//let client =  new LineConnect();
 
 client.startx().then(async (res) => {
 	
